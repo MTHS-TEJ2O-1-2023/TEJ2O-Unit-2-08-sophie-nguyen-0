07b62088260aeaf81e02c8ebe7346ff2649d606f
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2023-10-10 17:01:02 +0000

    py vers

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,6 +1,6 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Sophie
+Created on: Oct 2023
 This module is a Micro:bit MicroPython program that changes the color of a RGB LED
 """
 
@@ -14,36 +14,35 @@ while True:
         # red
         pin16.write_digital(1)
         display.scroll("Red")
-        sleep(1)
+        sleep(1000)
         pin16.write_digital(0)
         # green
         pin15.write_digital(1)
         display.scroll("green")
-        sleep(1)
+        sleep(1000)
         pin15.write_digital(0)
         # blue
         pin14.write_digital(1)
         display.scroll("blue")
-        sleep(1)
+        sleep(1000)
         # magenta
         pin16.write_digital(1)
         display.scroll("magenta")
-        sleep(1)
+        sleep(1000)
         pin16.write_digital(0)
         # cyan
         pin15.write_digital(1)
         display.scroll("cyan")
-        sleep(1)
+        sleep(1000)
         pin14.write_digital(0)
         # yellow
         pin16.write_digital(1)
         display.scroll("yellow")
-        sleep(1)
+        sleep(1000)
         # white
         pin14.write_digital(1)
         display.scroll("white")
-        sleep(1)
+        sleep(1000)
         pin16.write_digital(0)
         pin15.write_digital(0)
         pin14.write_digital(0)
-
